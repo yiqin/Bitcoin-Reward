a04acfef95f392e59b5162e9c7ca5ed342b0399d
yiqin <yiqin.mems@gmail.com>
2014-09-13 08:08:18 -0400

    Add BitcoinRewarding.h

---
+++ BitcoinRewarding/BitcoinRewarding.h
@@ -0,0 +1,13 @@
+//
+//  BitcoinRewarding.h
+//  Bitcoin-Reward
+//
+//  Created by yiqin on 9/13/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BitcoinRewarding : NSObject
+
+@end
---
+++ BitcoinRewarding/BitcoinRewarding.m
@@ -0,0 +1,13 @@
+//
+//  BitcoinRewarding.m
+//  Bitcoin-Reward
+//
+//  Created by yiqin on 9/13/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import "BitcoinRewarding.h"
+
+@implementation BitcoinRewarding
+
+@end
