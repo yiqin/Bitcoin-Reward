d028b8196857b299a8cb1323228292061e6f2499
yiqin <yiqin.mems@gmail.com>
2014-09-13 02:19:55 -0400

    init Bitcoin-Reward

---
+++ Bitcoin-Reward/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bitcoin-Reward
+//
+//  Created by yiqin on 9/13/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bitcoin-Reward/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Bitcoin-Reward
+//
+//  Created by yiqin on 9/13/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
